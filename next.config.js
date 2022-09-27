97304385c9bfe8fe5277bd6d5ddbb45995c32082
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-09-28 00:13:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    mongoDbPath:
-      'mongodb+srv://popovds:65D89YtDGhkLeVzN@auth-cluster.j4s4vys.mongodb.net/?retryWrites=true&w=majority',
-  },
+  //env: {
+   // mongoDbPath:
+    //  'mongodb+srv://popovds:65D89YtDGhkLeVzN@auth-cluster.j4s4vys.mongodb.net/?retryWrites=true&w=majority',
+ // },
 };
 
 module.exports = nextConfig;
