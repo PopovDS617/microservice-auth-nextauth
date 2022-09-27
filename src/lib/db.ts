29ab80652dd2d0e6322ab5a190fc779aaa3d3186
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-09-28 00:23:43 +0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
 export const connectToMongo = async () => {
-  const client = await MongoClient.connect(process.env.mongoDbPath);
+  const client = await MongoClient.connect(`${process.env.mongoDbPath}`);
   return client;
 };
